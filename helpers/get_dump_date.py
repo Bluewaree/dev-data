9302ebd861c775cef236f9770c93c4620451b3f3
blue <belkihelmehdi2@gmail.com>
2019-06-27 12:49:22 +0100

    Update get dump date function

--- helpers/get_dump_date.py
+++ helpers/get_dump_date.py
@@ -5,9 +5,9 @@ sys.path.append('..')
 import constants.constants as const
 
 # Returns the first date if first time run, otherwise returns from last registered date
-def get_dump_date(database_type):
+def get_dump_date(database_type,archives_base_folder):
     dump_date = const.DUMPS_START_DATE[database_type]
-    dump_date_file_path = os.path.join('..',const.DUMPS_DATE_PATH[database_type])
+    dump_date_file_path = os.path.join(archives_base_folder,f'{database_type}-dump-date.txt')
 
     if os.path.exists(dump_date_file_path): 
         with open(dump_date_file_path) as file:
