4747f29ab0880783c1b6546cd0f7d2b93801a000
blue <belkihelmehdi2@gmail.com>
2019-07-05 11:50:36 +0100

    Change get mysql table names helper

--- helpers/get_mysql_table_names.py
+++ helpers/get_mysql_table_names.py
@@ -3,6 +3,9 @@ import os
 def get_mysql_table_names(mysql_dump_folder):
 
     mysql_table_names = list()
-    for (dirpath, dirnames, filenames) in os.walk(mysql_dump_folder):
-        mysql_table_names += [file.replace(".csv","") for file in filenames if file.endswith(".csv")]
+    table_names_file = os.path.join(mysql_dump_folder, 'ORDER')
+    with open(table_names_file, 'r') as file:
+        for table_name in file:
+            if "commit" not in table_name:
+                mysql_table_names.append(table_name.rstrip('\n'))
     return mysql_table_names
