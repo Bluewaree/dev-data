c086352deb6b5825e8a867de787326351de98726
blue <belkihelmehdi2@gmail.com>
2019-07-10 12:30:40 +0100

    Add update/get users functions

--- database/mysql.py
+++ database/mysql.py
@@ -1,5 +1,10 @@
 from configparser import ConfigParser
 import MySQLdb
+import sys
+
+sys.path.append('..')
+import constants.constants as const
+from helpers.format_string import format_string
 
 config = ConfigParser()
 config.read('config.ini')
@@ -25,6 +30,8 @@ class MySQL(object):
 
     def restore_db(self, csv_file, table_name):
         cursor = self._db.cursor()
+        cursor.execute("SELECT @@foreign_key_checks;")
+        print(cursor.fetchone())
         cursor.execute("load data local infile '{0}' \
                         into table {1} \
                         fields terminated by ',' \
@@ -36,25 +43,41 @@ class MySQL(object):
     def commit(self):
         self._db.commit()
 
-    def update_users(self, users):
+    def update_users(self, users, database_documents_type):
         cursor = self._db.cursor()
+        cursor.execute("SET GLOBAL range_optimizer_max_mem_size=0;")
+        cursor.execute("show variables like \"range_optimizer_max_mem_size\"")
+        print(cursor.fetchone())
         query = ""
         email_case = "email = case"
         name_case = "name = case"
         logins = []
         for user in users:
-            user = user['data']
-            email_case += f" when login = {user['login']} then {user['email']}"
-            name_case += f" when login = {user['login']} then {user['name']}"
-            logins.append(user['login'])
+            if database_documents_type == const.MONGO:
+                user = user['data']
+            login = format_string(user['login'])
+            email = format_string(user['email'])
+            name = format_string(user['name'])
+            email_case += f" when login = %s then %s" % (login,email)
+            name_case += f" when login = %s then %s" % (login,name)
+            logins.append(login)
         email_case += " else email end"
         name_case += " else name end"
         logins = ','.join(logins)
         query = f"update users set {email_case}, {name_case} where login in ({logins})"
+        print("---- Query Ready ------")
         cursor.execute(query)
+        print("---- Query ended -----")
         cursor.close() 
         
     def execute_file(self,file_to_execute):
         cursor = self._db.cursor()
         cursor.execute(file_to_execute)
         cursor.close() 
+
+    def get_all_users(self):
+        cursor = self._db.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute("SELECT login,name,email FROM users limit 100000")
+        users = cursor.fetchall()
+        cursor.close()
+        return users
