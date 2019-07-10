2b309745efbc89f408eb8be3332beb775e79d313
blue <belkihelmehdi2@gmail.com>
2019-07-10 12:31:30 +0100

    Add helper for getting dump folder endpoint

---
+++ helpers/get_dump_folder_endpoint.py
@@ -0,0 +1,14 @@
+import os
+
+def get_dump_folder_endpoint(archives_base_folder,database_type,dump_date):
+    dump_enterence_folder = os.path.join(archives_base_folder,f'{database_type}-dump-{dump_date}')
+
+    for (dirpath, dirnames, filenames) in os.walk(dump_enterence_folder):
+        for file in filenames:
+            dump_folder_containing_files = dirpath
+            dump_folder_containing_files = dump_folder_containing_files.rsplit(dump_enterence_folder,1)[1]
+            break           
+         
+    dump_folder_path = dump_enterence_folder + dump_folder_containing_files
+
+    return dump_folder_path
