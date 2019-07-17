cb9c0462d2f03d0de3bbab016d880435c98e22c0
Blue <belkihelmehdi2@gmail.com>
2019-07-17 17:53:42 +0100

    Fix previous dump date issue

--- helpers/get_previous_dump_date.py
+++ helpers/get_previous_dump_date.py
@@ -8,7 +8,7 @@ import constants.constants as const
 
 # Returns the first date if first time run, otherwise returns from last registered date
 def get_previous_dump_date(database_type,archives_base_folder):
-    dump_date = const.DUMPS_START_DATE[database_type]
+    previous_dump_date = const.DUMPS_START_DATE[database_type]
     dump_date_file_path = os.path.join(archives_base_folder,f'{database_type}-dump-date.txt')
 
     if os.path.exists(dump_date_file_path): 
@@ -19,4 +19,4 @@ def get_previous_dump_date(database_type,archives_base_folder):
             else: # Last month if mysql
                 previous_dump_date = (datetime.strptime(dump_date, "%Y-%m-%d").date() - relativedelta(months=1)).replace(day=1)
 
-    return dump_date
+    return previous_dump_date
