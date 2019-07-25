e3d53aae264f1fe92bca751ff0de3b1ab04534f6
Blue <belkihelmehdi2@gmail.com>
2019-07-25 15:56:39 +0100

    Add next date bug

--- helpers/set_dump_date.py
+++ helpers/set_dump_date.py
@@ -11,9 +11,9 @@ def set_dump_date(database_type,archives_base_folder,current_dump_date):
     dump_date_file_path = os.path.join(archives_base_folder,f'{database_type}-dump-date.txt')
     
     if database_type==const.MONGO: # Add 1 day if Mongo dump
-        next_dump_date = datetime.strptime(current_dump_date, "%Y-%m-%d").date() + timedelta(days=1)
+        next_dump_date = datetime.strptime(current_dump_date, "%Y-%m-%d").date()
     else: # Add 1 month if Mysql dump
-        next_dump_date = (datetime.strptime(current_dump_date, "%Y-%m-%d").date() + relativedelta(months=1)).replace(day=1)
+        next_dump_date = (datetime.strptime(current_dump_date, "%Y-%m-%d").date()
 
     with open(dump_date_file_path,"w+") as file:
         file.write(next_dump_date.strftime("%Y-%m-%d"))
