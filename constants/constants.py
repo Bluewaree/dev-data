c4681bb083254ad582f7406f3e554b8539f22e82
Blue <belkihelmehdi2@gmail.com>
2019-08-02 12:09:09 +0100

    Change mysql dump start date

--- constants/constants.py
+++ constants/constants.py
@@ -5,7 +5,7 @@ MYSQL = 'mysql'
 MONGO_DAILY_DUMPS_START_DATE = '2015-12-02'
 DUMPS_START_DATE = {
     MONGO: '2015-08-04',
-    MYSQL: '2015-09-25'
+    MYSQL: '2019-06-01'
 }
 MONGO_MONTHLY_DUMPS_URLS = 'http://ghtorrent-downloads.ewi.tudelft.nl/mongo-full/'
 MONGO_DAILY_DUMPS_URL = 'http://ghtorrent-downloads.ewi.tudelft.nl/mongo-daily/mongo-dump-'
