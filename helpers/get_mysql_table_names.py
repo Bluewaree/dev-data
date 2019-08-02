8a2ee48edbd72ec79a7af17d197ac3805a593c05
Blue <belkihelmehdi2@gmail.com>
2019-08-02 12:10:03 +0100

    Load only the users

--- helpers/get_mysql_table_names.py
+++ helpers/get_mysql_table_names.py
@@ -3,7 +3,7 @@ import os
 def get_mysql_table_names(mysql_dump_folder):
     mysql_table_names = list()
     for filename in os.listdir(mysql_dump_folder):
-        if "csv" in filename and "commit" not in filename:
+        if "users" in filename:
             mysql_table_names.append(filename.replace('.csv',''))  
     mysql_table_names.sort(key=lambda filename: os.path.getsize(os.path.join(mysql_dump_folder,f"{filename}.csv")))
     return mysql_table_names
